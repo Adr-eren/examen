5ffd288ca5a26da1aac0d1beaa400653b1e642ac
Adr-eren <82550551+Adr-eren@users.noreply.github.com>
2021-05-12 21:36:18 -0500

    Add files via upload

---
+++ Ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*Garcia Hernandez Adriana
+    12 MAY 21
+    */
+
+    float a, b, c;
+    printf("\n\n Programa 2");
+    printf("\n\n Suma de numeros enteros");
+    printf("\n\n Dame el valor entero de a:");
+    scanf("%f", &a);
+    printf("\n\n Dame el valor entero de b:");
+    scanf("%f", &b);
+    c= a + b;
+    printf("\n El producto es %d", c);
+    return 0;
+}
